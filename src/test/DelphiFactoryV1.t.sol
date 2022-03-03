4b51a51e1bba37efbb62039a679d3d409e2dfa0e
Ben Clabby <benclabby@gmail.com>
2022-03-03 10:48:42 -0500

    New tests

--- src/test/DelphiFactoryV1.t.sol
+++ src/test/DelphiFactoryV1.t.sol
@@ -6,6 +6,7 @@ import "../DelphiFactoryV1.sol";
 
 contract DelphiFactoryV1Test is DSTest {
     DelphiFactoryV1 factory;
+    DelphiOracleV1 baseOracle;
 
     function setUp() public {
         address[] memory aggregators = _getLinkAggregators();
@@ -13,14 +14,12 @@ contract DelphiFactoryV1Test is DSTest {
         // Create Factory
         factory = new DelphiFactoryV1(aggregators);
         emit log_named_address("Factory", address(factory));
-    }
 
-    // -----------------------------
-    // ORACLE CREATION
-    // -----------------------------
+        // Check that all aggregators passed to constructor are allowed
+        for (uint8 i = 0; i < aggregators.length; i++) {
+            assertTrue(factory.linkAggregators(aggregators[i]));
+        }
 
-    // Test is pinned to block #14305846
-    function testOneVariableOracle() public {
         uint256[] memory expressions = new uint256[](5);
         // x*2
         expressions[0] = 6;
@@ -29,12 +28,18 @@ contract DelphiFactoryV1Test is DSTest {
         expressions[3] = 0;
         expressions[4] = 2;
 
-        address[] memory aggregators = _getLinkAggregators();
-
         // Deploy Oracle
-        DelphiOracleV1 oracle = DelphiOracleV1(factory.createOracle("2xETH", aggregators, expressions));
+        baseOracle = DelphiOracleV1(factory.createOracle("2xETH", aggregators, expressions));
+        emit log_named_address("Base Oracle (2xETH)", address(baseOracle));
+    }
 
-        assertEq(oracle.getLatestValue(), 6000612530720000000000);
+    // -----------------------------
+    // ORACLE CREATION
+    // -----------------------------
+
+    // Test is pinned to block #14305846
+    function testOneVariableOracle() public {
+        assertEq(baseOracle.getLatestValue(), 6000612530720000000000);
     }
 
     // Test is pinned to block #14305846
@@ -79,7 +84,28 @@ contract DelphiFactoryV1Test is DSTest {
         assertEq(oracle.getLatestValue(), 737832013977270330932800);
     }
 
+    // Try to deploy two oracles with the same aggregators/expressions. Should fail.
     function testFailTwoOfAKind() public {
+        uint256[] memory expressions = new uint256[](5);
+        // x*y
+        expressions[0] = 6;
+        expressions[1] = 1;
+        expressions[2] = 0;
+        expressions[3] = 1;
+        expressions[4] = 1;
+
+        address[] memory aggregators = _getLinkAggregators();
+
+        // A second oracle that performs the same operation on the same set of aggregators as another cannot be deployed
+        factory.createOracle("ETHxBTC", aggregators, expressions);
+        factory.createOracle("ETHxBTC", aggregators, expressions);
+    }
+
+    // Try to create an oracle with an aggregator that isn't allowed. Should fail.
+    function testFailCreateOracleWithDisallowedAggregator() public {
+        address[] memory aggregators = new address[](1);
+        aggregators[0] = 0x8994115d287207144236c13Be5E2bDbf6357D9Fd;
+
         uint256[] memory expressions = new uint256[](5);
         // x*2
         expressions[0] = 6;
@@ -88,11 +114,20 @@ contract DelphiFactoryV1Test is DSTest {
         expressions[3] = 0;
         expressions[4] = 2;
 
-        address[] memory aggregators = _getLinkAggregators();
+        // Deploy Oracle, should fail because the AMZN/USD aggregator is not allowed
+        factory.createOracle("2xAMZN", aggregators, expressions);
+    }
 
-        // Deploy two oracles with the same aggregators/expressions. Should fail.
-        DelphiOracleV1(factory.createOracle("2xETH", aggregators, expressions));
-        DelphiOracleV1(factory.createOracle("2xETH", aggregators, expressions));
+    // Try to re-initialize an oracle. Should fail.
+    function testFailDoubleInit() public {
+        uint256[] memory expressions = new uint256[](1);
+
+        baseOracle.init(
+            "2xETH",
+            address(this),
+            _getLinkAggregators(),
+            expressions
+        );
     }
 
     // -----------------------------
@@ -100,45 +135,23 @@ contract DelphiFactoryV1Test is DSTest {
     // -----------------------------
 
     function testEndorseOracle() public {
-        uint256[] memory expressions = new uint256[](5);
-        // x*2
-        expressions[0] = 6;
-        expressions[1] = 1;
-        expressions[2] = 0;
-        expressions[3] = 0;
-        expressions[4] = 2;
-
-        address[] memory aggregators = _getLinkAggregators();
-
-        // Deploy two oracles with the same aggregators/expressions. Should fail.
-        address oracleAddress = factory.createOracle("2xETH", aggregators, expressions);
+        address oracleAddress = address(baseOracle);
 
         factory.setEndorsed(oracleAddress, true);
         assertTrue(factory.endorsed(oracleAddress));
 
         factory.setEndorsed(oracleAddress, false);
-        assertFalse(factory.endorsed(oracleAddress));
+        assertTrue(!factory.endorsed(oracleAddress));
     }
 
     function testAllowAggregator() public {
-        uint256[] memory expressions = new uint256[](5);
-        // x*2
-        expressions[0] = 6;
-        expressions[1] = 1;
-        expressions[2] = 0;
-        expressions[3] = 0;
-        expressions[4] = 2;
-
         address[] memory aggregators = _getLinkAggregators();
 
-        // Deploy two oracles with the same aggregators/expressions. Should fail.
-        address oracleAddress = factory.createOracle("2xETH", aggregators, expressions);
-
         factory.setAllowAggregator(aggregators[0], true);
-        assertTrue(factory.linkAggregators()[aggregators[0]]);
+        assertTrue(factory.linkAggregators(aggregators[0]));
 
         factory.setAllowAggregator(aggregators[0], false);
-        assertTrue(factory.linkAggregators()[aggregators[0]]);
+        assertTrue(!factory.linkAggregators(aggregators[0]));
     }
 
     // -----------------------------
@@ -151,7 +164,5 @@ contract DelphiFactoryV1Test is DSTest {
         linkAggregators[0] = 0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419; // ETH/USD -> x
         linkAggregators[1] = 0xF4030086522a5bEEa4988F8cA5B36dbC97BeE88c; // BTC/USD -> y
         linkAggregators[2] = 0x2c1d072e956AFFC0D435Cb7AC38EF18d24d9127c; // LINK/USD -> z
-        // aggregators[0] = 0x47E1e89570689c13E723819bf633548d611D630C; // BTC Marketcap USD
-        // aggregators[1] = 0xAA2FE1324b84981832AafCf7Dc6E6Fe6cF124283; // ETH Marketcap USD
     }
 }
